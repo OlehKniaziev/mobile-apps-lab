048b365a8b046a3946fa117086b4943a6850de32
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2025-03-05 16:00:30 +0100

    Finished Lab2 tasks

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.Toolbar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.absoluteValue
@@ -13,8 +15,57 @@ import kotlin.math.sign
 class MainActivity : AppCompatActivity() {
     lateinit var mLayout: LinearLayout
     lateinit var mTitle: TextView
-    var mBoxes: MutableList<CheckBox> = mutableListOf()
-    var mButtons: MutableList<Button> = mutableListOf()
+    lateinit var mProgress: ProgressBar
+    val mBoxes: MutableList<CheckBox> = mutableListOf()
+    val mButtons: MutableList<Button> = mutableListOf()
+
+    val mTests = listOf<() -> Boolean>(
+        {
+            task11(4, 6) in 0.666665..0.666667 &&
+                task11(7, -6) in -1.1666667..-1.1666665
+        },
+        {
+                task12(7U, 6U) == "7 + 6 = 13" &&
+                        task12(12U, 15U) == "12 + 15 = 27"
+
+        },
+        {
+            task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
+                    !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
+                    !task13(6.0, -1.0f) && task13(1.0, 1.1f)
+
+        },
+        {
+            task14(-2, 5) == "-2 + 5 = 3" &&
+                    task14(-2, -5) == "-2 - 5 = -7"
+        },
+        {
+            task15("DOBRY") == 4 &&
+                    task15("barDzo dobry") == 5 &&
+                    task15("doStateczny") == 3 &&
+                    task15("Dopuszczający") == 2 &&
+                    task15("NIEDOSTATECZNY") == 1 &&
+                    task15("XYZ") == -1
+
+        },
+        {
+            task16(
+                mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                mapOf("A" to 1U, "B" to 2U)
+            ) == 2U
+                    &&
+                    task16(
+                        mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                        mapOf("F" to 1U, "G" to 2U)
+                    ) == 0U
+                    &&
+                    task16(
+                        mapOf("A" to 23U, "B" to 47U, "C" to 30U),
+                        mapOf("A" to 1U, "B" to 2U, "C" to 4U)
+                    ) == 7U
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,69 +80,46 @@ class MainActivity : AppCompatActivity() {
         mTitle.layoutParams = params
         mLayout.addView(mTitle)
 
+        mProgress = ProgressBar(
+            this,
+            null,
+            androidx.appcompat.R.attr.progressBarStyle,
+            androidx.appcompat.R.style.Widget_AppCompat_ProgressBar_Horizontal
+        )
+
         for (i in 1..6) {
+            val row = LinearLayout(this)
+            row.layoutParams = LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT
+            )
+            row.orientation = LinearLayout.HORIZONTAL
+
             val checkBox = CheckBox(this)
             checkBox.text = "Zadanie ${i}"
             checkBox.isEnabled = false
-            mLayout.addView(checkBox)
+
+            val button = Button(this).also {
+                it.text = "test"
+            }
+
+            button.setOnClickListener {
+                val idx = i - 1
+                val result = mTests[idx]()
+                if (result) {
+                    mBoxes[idx].isChecked = true
+                    mProgress.progress += Math.ceil(100.0 / mTests.count().toDouble()).toInt()
+                }
+            }
+
+            row.addView(checkBox)
+            row.addView(button)
+
+            mLayout.addView(row)
             mBoxes.add(checkBox)
         }
 
-        if (
-            task11(4, 6) in 0.666665..0.666667 &&
-            task11(7, -6) in -1.1666667..-1.1666665
-        ) {
-            mBoxes[0].isChecked = true
-        }
-
-        if (
-            task12(7U, 6U) == "7 + 6 = 13" &&
-            task12(12U, 15U) == "12 + 15 = 27"
-        ) {
-            mBoxes[1].isChecked = true
-        }
-
-        if (
-            task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
-            !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
-            !task13(6.0, -1.0f) && task13(1.0, 1.1f)
-        ) {
-            mBoxes[2].isChecked = true
-        }
-
-        if (
-            task14(-2, 5) == "-2 + 5 = 3" &&
-            task14(-2, -5) == "-2 - 5 = -7"
-        ) {
-            mBoxes[3].isChecked = true
-        }
-        if (
-            task15("DOBRY") == 4 &&
-            task15("barDzo dobry") == 5 &&
-            task15("doStateczny") == 3 &&
-            task15("Dopuszczający") == 2 &&
-            task15("NIEDOSTATECZNY") == 1 &&
-            task15("XYZ") == -1
-        ){
-            mBoxes[4].isChecked = true
-        }
-        if (task16(
-                mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                mapOf("A" to 1U, "B" to 2U)
-            ) == 2U
-            &&
-            task16(
-                mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                mapOf("F" to 1U, "G" to 2U)
-            ) == 0U
-            &&
-            task16(
-                mapOf("A" to 23U, "B" to 47U, "C" to 30U),
-                mapOf("A" to 1U, "B" to 2U, "C" to 4U)
-            ) == 7U
-        ) {
-            mBoxes[5].isChecked = true
-        }
+        mLayout.addView(mProgress)
     }
 
     // Wykonaj dzielenie niecałkowite parametru a przez b
